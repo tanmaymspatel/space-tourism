c0a0774e73f82908506ae65d7af494b5567e2633
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-02-27 16:06:11 +0530

    feat: Project setup and basic html structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="h-100 d-flex flex-column">
+      <header id="header">
+        header
+      </header>
+      <main className="main-content flex-grow-1">
+        main
+      </main>
+    </div>
+  );
+}
+
+export default App;
